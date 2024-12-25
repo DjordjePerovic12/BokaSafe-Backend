128b52d1d3ee291c6a4ceb84a0b013d746ef17bf
Djordje Perovic <djordje.perovic@amplitudo.me>
2024-12-25 16:36:47 +0100

    Removes secure from git ignore

--- config/firbease.js
+++ config/firbease.js
@@ -1,22 +1,8 @@
 const admin = require('firebase-admin');
-const dotenv = require('dotenv');
+const serviceAccount = require('../secure/serviceAccount.json'); // Adjust path as needed
 
-// Load environment variables
-dotenv.config();
-
-// Decode URL-safe Base64
-function decodeBase64UrlSafe(base64) {
-    base64 = base64.replace(/-/g, '+').replace(/_/g, '/');
-    return Buffer.from(base64, 'base64').toString('utf8');
-}
-
-// Parse the service account JSON
-const serviceAccountJson = decodeBase64UrlSafe(process.env.FIREBASE_SERVICE_ACCOUNT);
-const serviceAccount = JSON.parse(serviceAccountJson);
-
-// Initialize Firebase Admin SDK
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
+    credential: admin.credential.cert(serviceAccount)
+  });
 
 module.exports = admin;
