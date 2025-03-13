b1b3758abc509f5768376c5f8a76caf93d5860dd
Djordje Perovic <djordje.perovic@amplitudo.me>
2025-03-13 15:36:25 +0100

    Adds more file limit logic

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ if (!fs.existsSync(uploadsDir)) {
 
 app.use(cors(corsOptions));
 
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/uploads', express.static(uploadsDir));
-app.use(express.json({ limit: '15mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use((err, req, res, next) => {
     if (err instanceof multer.MulterError) {
         return res.status(400).json({ message: err.message });
