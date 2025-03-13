adee5f163654e32214cdd44969078ace62881e30
Djordje Perovic <djordje.perovic@amplitudo.me>
2025-03-13 15:33:55 +0100

    Refactors upload limit logic

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/uploads', express.static(uploadsDir));
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use((err, req, res, next) => {
     if (err instanceof multer.MulterError) {
         return res.status(400).json({ message: err.message });
